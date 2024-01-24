cefb76ec8ffc02f62f396984d52328cfa12c763e
Vishnu Varadhan <144381362+vishnuvvaradhan@users.noreply.github.com>
2024-01-23 20:34:15 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = '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'
-PINECONE_API_KEY = '7e7f3b01-c30f-4af5-95b1-eb25f9a6cbee'
+OPENAI_API_KEY = 'YOUR_API_KEY'
+PINECONE_API_KEY = 'YOUR_API_KEY'
 
 user_prompt = 'As a chatbot for a golf e-commerce site, provide assistance based on the closed prompt. For queries that are about golf products not in the closed prompt or totally unrelated to the previous conversation, reply with: "This chatbot only answers questions about golf products in our database.", use the list called context to understand the past questions the user asked to help you answer future questions.'
